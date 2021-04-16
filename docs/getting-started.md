51ff6146012b0e34f1e91e9097fa5cbc1c8a2c19
dabit3 <dabit3@gmail.com>
2021-04-15 19:16:40 -0500

    initial commit

---
+++ docs/getting-started.md
@@ -0,0 +1,18 @@
+---
+title: Master The Graph
+slug: /
+---
+
+Browse the latest documentation including API reference, articles, and sample code.
+
+## Hosted Service
+
+Build subgraphs or use existing subgraphs in a dApp
+
+[Read more](hosted/introduction)
+
+## Network
+
+Participate in the network as an Indexer, Curator, or Delegator
+
+[Read more](network/introduction)
---
+++ sidebars.js
@@ -0,0 +1,25 @@
+module.exports = {
+  docs: [
+    {
+      type: 'category',
+      label: 'Hosted Service',
+      items: [
+        'hosted/introduction',
+        'hosted/quick-start',
+        'hosted/define-a-subgraph',
+        'hosted/deploy-a-subgraph',
+        'hosted/query-the-graph',
+        'hosted/assemblyscript-api',
+        'hosted/graphql-api',
+        'hosted/versioning'
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Network',
+      items: [
+        'network/Introduction',
+      ],
+    }
+  ],
+};
