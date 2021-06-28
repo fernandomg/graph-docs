2615be3922c626485d9b5c5efe4f3f70dd2a0763
Martynas Prokopas <promaty@gmail.com>
2021-06-28 22:03:46 +0200

    lint ci/cd script

--- .github/scripts/npx-wait-on/config.js
+++ .github/scripts/npx-wait-on/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   headers: {
-    'accept': 'text/html'  // this avoids graphql endpoint error by returning the playground html
+    accept: 'text/html' // this avoids graphql endpoint error by returning the playground html
   }
 }
