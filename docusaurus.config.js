1fafbc93b42fcb64235b61e18f373c00aa059bf2
Iain McCown <iain@edgeandnode.com>
2021-07-13 13:59:48 -0500

    redirects for old routes (#84)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,45 @@ module.exports = {
     navbar: {},
     footer: {}
   },
-  plugins: [require.resolve('docusaurus-plugin-sass')],
+  plugins: [
+    require.resolve('docusaurus-plugin-sass'),
+    // redirect old docs routes to new ones
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        redirects: [
+          {
+            to: '/developer/quick-start',
+            from: '/quick-start'
+          },
+          {
+            to: '/developer/define-subgraph-hosted',
+            from: '/define-a-subgraph'
+          },
+          {
+            to: '/developer/deploy-subgraph-hosted',
+            from: '/deploy-a-subgraph'
+          },
+          {
+            to: '/developer/query-the-graph',
+            from: '/query-the-graph'
+          },
+          {
+            to: '/developer/assemblyscript-api',
+            from: '/assemblyscript-api'
+          },
+          {
+            to: '/developer/graphql-api',
+            from: '/graphql-api'
+          },
+          {
+            to: '/indexing',
+            from: '/network'
+          },
+        ],
+      }
+    ],
+  ],
   presets: [
     [
       '@docusaurus/preset-classic',
